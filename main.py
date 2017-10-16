5188fc9d5c5bc722ed167ab676df860aa7fa67a3
David A. Foote <david.a.foote@gmail.com>
2017-10-15 20:59:07 -0500

    Still only a blank display page for individual blogs

--- main.py
+++ main.py
@@ -24,16 +24,16 @@ def index():
 
 @app.route('/blog', methods=['GET'])
 def new_blog():
-    blog_ids = request.args.get('ids')
-    if blog_ids == None: 
+    id = request.args.get('id')
+    if id == None: 
         blogs = Blog.query.all()
         return render_template("blog.html", blogs=blogs)
     else:
-        individual_blog = Blog.query.get(blog_ids)
-        render_template('individual_blog.html', + blog_ids)
+        individual_blog = Blog.query.get(id)
+        return render_template('individual_blog.html', title="Build a Blog", blog=individual_blog)
     
     
-@app.route('/blog', methods=['POST'])
+@app.route('/blog', methods=['POST', 'GET'])
 def display_blog_form():
     
     blog_title = request.form['title']
@@ -48,14 +48,14 @@ def display_blog_form():
         blog = Blog(blog_title, blog_body)
         db.session.add(blog)
         db.session.commit()   
-        return redirect('/blog?id=' + blog.id)  
+        return render_template('/blog?id=' + blog.id)  
     else:
         return render_template('new_post.html', blog_title_error=blog_title_error, blog_body_error=blog_body_error)
       
 
 @app.route('/new_post', methods=['GET'])
-def start_post():
-     return render_template("new_post.html")
+def new_post():
+     return render_template('new_post.html')
 
 @app.route('/individual_blog', methods=['POST', 'GET'])
 def individual_blog():
@@ -64,10 +64,10 @@ def individual_blog():
         add_entry = Blog(new_blog)
         db.session.add(add_entry)
         db.session.commit()
-        return redirect('/individual_blog?id=' + new_blog.id)
+        return redirect('/blog?id=' + blog.id)
     else:
-         return render_template('new_post.html') 
-          
+        return render_template('new_post.html') 
+        
     
 if __name__ == '__main__':
     app.secret_key = "secret"
