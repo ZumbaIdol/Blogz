adff8e39e9598b74ada8de251da96947c46f333e
David A. Foote <david.a.foote@gmail.com>
2017-10-19 17:57:56 -0500

    Getting close

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz2:happy@localhost:8889/blogz2'
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
       
     id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(120))
     body = db.Column(db.String(2000))
   
-    def __init__(self, title, body):
+    def __init__(self, owner, title, body):
+        self.owner = owner
         self.title = title
-        self.body = body      
+        self.body = body 
 
 
-@app.route('/', methods=['POST', 'GET'])
+class User(db.Model):
+      
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.Column(db.String(2000))
+  
+    def __init__(self, username, password, blogs):
+        self.username = username
+        self.password = password
+        self.blogs = blogs  
+
+         
+@app.route('/')
 def index():
-    return redirect('/blog') 
+    author_username = request.args.get('owner_id')
+    if author_username == None:
+        blogs = Blog.query.all()
+        return render_template("blog.html", blogs=blogs)
+    else:
+        individual_blog = Blog.query.get(id)
+        return render_template('individual_blog.html', title="Build a Blog", individual_blog=individual_blog)
+
+    endpoints_without_login = ['login', 'signup']
 
 
 @app.route('/blog', methods=['GET'])
@@ -66,7 +89,95 @@ def individual_blog():
         db.session.commit()
         return redirect('/blog?id=' + blog.id)
     else:
-        return render_template('new_post.html') 
+        return render_template('new_post.html')
+
+@app.route('/signup', methods=['POST', 'GET']) 
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']  
+        confirm_password = request.form['confirm_password']
+        username_error = ''
+        password_error = ''
+        confirm_password_error = ''
+        existing_user = User.query.filter_by(username=username)
+        if not existing_user:
+            new_user = User(username, password)
+            return redirect('/signup')
+        username_db_count = User.query.filter_by(username=username)
+        if username_db_count > 0:
+            flash('Username already exists')
+            return redirect('/signup')
+        if password != confirm:
+            flash('Passwords must match')
+            return redirect('/signup')
+
+        user = User(username=username, password=password)
+        if username == '':
+            flash('Cannot leave fields empty', category='message')
+        if password == '':
+            flash('Cannot leave fields empty', category='message')
+        if username == "" or len(username) < 3 or len(username) > 20:
+            flash('Value out of range (3-20)', category='message')
+        if password == "" or len(password) < 3 or len(password) > 20:
+            flash('Value out of range (3-20)', category='message')
+        if confirm_password == "" or len(confirm_password) < 3 or len(confirm_password) > 20:
+            flash('Passwords must match', category='message')
+        db.session.add(user)
+        db.session.commit()
+        session['user'] = user.username
+        return redirect('/')
+    else:
+        return render_template('signup.html')
+
+                       
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'index', 'blog']
+    if request.endpoint not in allowed_routes and 'user' not in session:
+        return redirect('/login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = User.query.filter_by(username=username)
+    if users.count() == 1:
+        users = users.first()
+        if password == users.password:
+            session['user'] = user.username
+            flash('Welcome back', + user.username)
+            return redirect('/')
+    flash('Incorrect username or password')
+    return redirect('/login')
+                
+
+
+@app.route('/singleUser', methods=['GET'])
+def singleUser():
+    owner_id = request.form['owner_id']
+    add_entry = Blog(new_blog)
+    db.session.add(add_entry)
+    db.session.commit()
+    return redirect('/user?Id=' + user.id)
+    
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    del session['username']
+    return redirect('/')
+   
+
+# @app.route('/index', methods=['POST'])
+# def index():
+#     return render_template('index.html')
+    
+
+
         
     
 if __name__ == '__main__':
